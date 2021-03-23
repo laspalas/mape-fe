f262ad843a74b8d37a4b17ded20e25ef773fb371
Luka <luka.jovanovic@bioclinica.com>
2021-03-23 17:33:41 +0100

    zasto kibs

--- src/pages/ZastoKibs.js
+++ src/pages/ZastoKibs.js
@@ -10,11 +10,11 @@ import { Line, Pie, Doughnut, Bar, Radar, Polar } from 'react-chartjs-2';
 import Page from 'components/Page';
 
 import MathJax from 'react-mathjax';
-
+import slika from '../assets/img/products/product_640-4.jpg';
 
 const first_formula = `KIBS_{j}=\\frac{max} {\\overline {w_{ij}}} \\sum\\nolimits_{i=1}^l= \\overline {r_{ij}} \\ \\overline {w_{ij}}`;
 const second_formula = `\\sum_{i=1}^l \\overline {w_{ij}} = 1 `;
-const third_formula = `\\leq\\frac{1} {l-1} \\sum_{i=1}^l ({l-1})\\overline {w_{ij}} = 1 \\geq`; 
+const third_formula = `\\leq\\frac{1} {l-1} \\sum_{i=1}^l ({l-1})\\overline {w_{ij}} = 1 \\geq`;
 const forth_formula = `\\sum_{i=1}^l \\overline {w_{ij}} = 1 `;
 const description_1 = `Gde\\ je:`;
 const description_2 = `I\\ =\\ broj\\ indikatora`;
@@ -24,8 +24,6 @@ const description_5 = `w\\ =\\ težina`;
 const description_6 = `m\\ =\\ \\{alkolol,\\ brzina,\\ zaštitni sistem,\\ upotreba\\ dnevnih\\ svetala\\}`;
 const description_7 = `L\\ =\\ donja\\ granica`;
 const description_8 = `U\\ =\\ gornja\\ granica`;
-
-
 
 const ChartPage = () => {
   return (
@@ -37,49 +35,15 @@ const ChartPage = () => {
               <CardHeader>
                 <h3>Metodologija</h3>
               </CardHeader>
-              <CardBody>
-                <ol>
-                  <li>
-                    Selecting the appropriate indicators to combine them in an
-                    index.
-                  </li>
-                  <li>Collecting the data on indicators.</li>
-                  <li>Making of data analyses</li>
-                  <li>Assigning the weights to each indicator</li>
-                  <li>Aggregating the values of indicators.</li>
-                  <li>Testing the robustness of the index</li>
-                  <li>
-                    Computing, evaluating and visualizing the scores of the
-                    final index.
-                  </li>
-                </ol>
-                <h5>Weighting method</h5>
-                <h5>Aggregating indicators</h5>
-                <h5>Index methodology</h5>
-              </CardBody>
+              <CardBody style={{ minHeight: '700px' }}>Text</CardBody>
             </Card>
           </Col>
           <Col>
             <Card>
-              <CardBody>
-                <Row>
-                  <Col>
-                    <MathJax.Node formula={first_formula} />
-                    <MathJax.Node formula={second_formula} />
-                    <MathJax.Node formula={third_formula} />
-                    <MathJax.Node formula={forth_formula} />
-                  </Col>
-                  <Col>
-                    <MathJax.Node formula={description_1} />
-                    <MathJax.Node formula={description_2} />
-                    <MathJax.Node formula={description_3} />
-                    <MathJax.Node formula={description_4} />
-                    <MathJax.Node formula={description_5} />
-                    <MathJax.Node formula={description_6} />
-                    <MathJax.Node formula={description_7} />
-                    <MathJax.Node formula={description_8} />
-                  </Col>
-                </Row>
+              <CardBody
+                style={{ padding: 0, minHeight: '765px', height: '765px' }}
+              >
+                <img width="100%" height="100%" src={slika}></img>
               </CardBody>
             </Card>
           </Col>
