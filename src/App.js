102514f263495d1dbdc4b70159af44bba79c41e4
Luka <luka.jovanovic@bioclinica.com>
2021-07-13 09:10:51 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -1,57 +1,57 @@
 import { STATE_LOGIN, STATE_SIGNUP } from 'components/AuthForm';
-import GAListener from 'components/GAListener';
 import { EmptyLayout, LayoutRoute, MainLayout } from 'components/Layout';
 import PageSpinner from 'components/PageSpinner';
 import AuthPage from 'pages/AuthPage';
 import React from 'react';
 import componentQueries from 'react-component-queries';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './styles/reduction.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NosiociProjekta from './pages/NosiociProjekta';
 import ModelKibs from './pages/ModelKibs';
+import Mape  from 'pages/Mape';
 
 const PracenjeStanjaKibsa = React.lazy(() =>
   import('pages/PracenjeStanjaKibsa'),
 );
-const Mape = React.lazy(() => import('pages/Mape'))
 const ZastoKibs = React.lazy(() => import('pages/ZastoKibs'));
 const OsnovneInformacije = React.lazy(() => import('pages/OsnovneInformacije'));
 const InputGroupPage = React.lazy(() => import('pages/InputGroupPage'));
 const BazaPodataka = React.lazy(() => import('pages/BazePodataka'));
 const AdminPage = React.lazy(() => import('pages/Admin'));
 
-
-const getBasename = () => {
-  return `/${process.env.PUBLIC_URL.split('/').pop()}`;
-};
+// const getBasename = () => {
+//   return `/${process.env.PUBLIC_URL.split('/').pop()}`;
+// };
 
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter basename={getBasename()}>
-        <GAListener>
-          <Switch>
-            <MainLayout breakpoint={this.props.breakpoint}>
-              <React.Suspense fallback={<PageSpinner />}>
-                <Route exact path="/" component={OsnovneInformacije} />
-                <Route
-                  exact
-                  path="/kibspracenje"
-                  component={PracenjeStanjaKibsa}
-                />
-                <Route exact path="/mape" component={Mape} />
-                <Route exact path="/model_kibs" component={ModelKibs} />
-                <Route exact path="/zasto_kibs" component={ZastoKibs} />
-                <Route exact path="/baza" component={BazaPodataka} />
-                <Route exact path="/nosioci_aktivnosti" component={NosiociProjekta} />
-                <Route exact path="/admin" component={AdminPage} />
-              </React.Suspense>
-            </MainLayout>
-            <Redirect to="/" />
-          </Switch>
-        </GAListener>
-      </BrowserRouter>
+      <HashRouter base={'/'}>
+        <Switch>
+          <MainLayout breakpoint={this.props.breakpoint}>
+            <React.Suspense fallback={<PageSpinner />}>
+              <Route exact path="/" component={OsnovneInformacije} />
+              <Route
+                exact
+                path="/kibspracenje"
+                component={PracenjeStanjaKibsa}
+              />
+              <Route exact path="/mape" component={Mape} />
+              <Route exact path="/model_kibs" component={ModelKibs} />
+              <Route exact path="/zasto_kibs" component={ZastoKibs} />
+              <Route exact path="/baza" component={BazaPodataka} />
+              <Route
+                exact
+                path="/nosioci_aktivnosti"
+                component={NosiociProjekta}
+              />
+              <Route exact path="/admin" component={AdminPage} />
+            </React.Suspense>
+          </MainLayout>
+          <Redirect to="/" />
+        </Switch>
+      </HashRouter>
     );
   }
 }
